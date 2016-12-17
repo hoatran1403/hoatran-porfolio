92a80e010fbca608be55c8ae6731f91bcdccc9ea
Hoa Tran <hoatranhcmiu@gmail.com>
2016-12-17 15:09:05 +0700

    Add Gulp browser-sync and live reload

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,20 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
 
-gulp.task('default', function(){
+gulp.task('default', ['browserSync'],function(){
 
 });
+
+
+gulp.task('browserSync', function(){
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+  //watch for file changes
+  gulp.watch(['src/**/*','index.html'],function(){
+    //reload browser on file changes
+    browserSync.reload();
+  });
+})
