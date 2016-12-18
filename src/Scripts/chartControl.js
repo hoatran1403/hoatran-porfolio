23e8c1e208adf0136b79504d57e1269ee994942c
Hoa Tran <hoatranhcmiu@gmail.com>
2016-12-18 12:17:18 +0700

    add full stack chart

--- src/Scripts/chartControl.js
+++ src/Scripts/chartControl.js
@@ -1,6 +1,30 @@
 $(document).ready(function(){
-  var ctx = $('#backEndChart');
-  var backEndChart = new Chart(ctx, {
+  // Chart global options
+  Chart.defaults.global.legend.display = false;
+
+
+  var fullStackCtx = $('#fullStackChart');
+  var fullStackChart = new Chart(fullStackCtx,{
+    type:'pie',
+    data: {
+      labels:['Back End', 'Front End'],
+      datasets:[{
+        data:[40,60],
+        backgroundColor: [
+          "rgba(0,0,0,.5)",
+          "rgba(0,0,0,.85)"
+        ],
+        hoverBackgroundColor:[
+          "rgba(0,0,0,.7)",
+          "rgba(0,0,0,1)"
+        ]
+      }]
+    }
+
+  });
+
+  var backEndCtx = $('#backEndChart');
+  var backEndChart = new Chart(backEndCtx, {
     type: 'bar',
     data:{
       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
@@ -8,25 +32,19 @@ $(document).ready(function(){
         label: '# of Votes',
         data: [12, 18, 3, 5, 2, 3],
         backgroundColor:[
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255,99,132,1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1
+          'rgba(255, 99, 132, 0.7)',
+          'rgba(54, 162, 235, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(75, 192, 192, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(255, 159, 64, 0.7)'
+        ]
       }]
     },
     options:{
+      legend:{
+        display:false
+      },
       scales:{
         yAxes: [{
           ticks: {
