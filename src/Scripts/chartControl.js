9600da3e1d4e01aacc3ba9b0d6a0805e5c1d6b13
Hoa Tran <hoatranhcmiu@gmail.com>
2016-12-18 22:13:21 +0700

    commit all node modules

--- src/Scripts/chartControl.js
+++ src/Scripts/chartControl.js
@@ -22,38 +22,4 @@ $(document).ready(function(){
     }
 
   });
-
-  var backEndCtx = $('#backEndChart');
-  var backEndChart = new Chart(backEndCtx, {
-    type: 'bar',
-    data:{
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 18, 3, 5, 2, 3],
-        backgroundColor:[
-          'rgba(255, 99, 132, 0.7)',
-          'rgba(54, 162, 235, 0.7)',
-          'rgba(255, 206, 86, 0.7)',
-          'rgba(75, 192, 192, 0.7)',
-          'rgba(153, 102, 255, 0.7)',
-          'rgba(255, 159, 64, 0.7)'
-        ]
-      }]
-    },
-    options:{
-      legend:{
-        display:false
-      },
-      scales:{
-        yAxes: [{
-          ticks: {
-            beginAtZero: true
-          }
-        }]
-      }
-    }
-  })
-
-
 });
